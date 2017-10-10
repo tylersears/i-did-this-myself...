26034128d22a3edf5cf854df2a946a408b9a251b
coolguy284 <32657659+coolguy284@users.noreply.github.com>
2017-10-10 10:11:04 -0400

    Update countdowntime.js

--- countdowntime.js
+++ countdowntime.js
@@ -1,5 +1,6 @@
 var id, cd, dif, sd;
+date = new Date();
 id = new Date(2017, 10, 03, 0, 0, 0);
-cd = new Date(year(), month(), day(), 0, 0, 0);
+cd = new Date(date.getFullYear(), date.getMonth()+1, date.getDate(), 0, 0, 0);
 dif = Math.round(Math.abs((id.getTime() - cd.getTime())/86400000));
 sd = 60-dif*3;
