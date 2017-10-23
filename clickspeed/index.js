5d093d6cb1fdb59c6c9c0f37ca849af4d256f232
coolguy284 <32657659+coolguy284@users.noreply.github.com>
2017-10-23 11:57:02 -0400

    Update index.js

--- clickspeed/index.js
+++ clickspeed/index.js
@@ -40,11 +40,34 @@ function Loop() {
   clicks1s.innerHTML = 'Clicks in last 1s: ' + l1s;
   clicks1ps.innerHTML = 'Clicks per second: ' + l1sp;
   clicks1m.innerHTML = 'Max Clicks per second: ' + l1m;
-  document.cookie = l10m + '; ' + l1m;
+  setCookie('l10m', parseInt(l10m * 10) + '', 365);
+  setCookie('l1m', l1m + '', 365);
+  cookie.innerHTML = document.cookie;
 }
 function ClearCookie() {
   l10m = 0;
   l1m = 0;
+}
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
 Loop();
 setInterval(Loop, 10);
@@ -70,10 +93,9 @@ draw = function() {
 };
 try {
   cookie.innerHTML = document.cookie;
-  a = document.cookie.split('; ');
-  if (a.length > 1) {
+  a = [getCookie('l10m'), getCookie('l1m')];
   l10m = parseInt(a[0])/10;
-  l1m = parseInt(a[1]);}
+  l1m = parseInt(a[1]);
 } catch (e) {}
 var canvas = document.getElementById("mycanvas"); 
 var processingInstance = new Processing(canvas, sketchProc);
