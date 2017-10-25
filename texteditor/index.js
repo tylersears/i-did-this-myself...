1dd78485437ee10e05fa0bfd16628893913907a2
coolguy284 <32657659+coolguy284@users.noreply.github.com>
2017-10-25 13:13:02 -0400

    Update index.js

--- texteditor/index.js
+++ texteditor/index.js
@@ -2,19 +2,25 @@ var sketchProc = function(processingInstance) {
   with (processingInstance) {
 //begin processingjs
 size(600, 400, P2D);
-textv = 'para\ngraph';
+textv = '';
 textAlign(LEFT, TOP);
 keyPressed = function () {
-  if (keyCode != 8) {
-    textv += String.fromCharCode(key);
+  if (keyCode != 127) {
+    if ((keyCode != 16) && (keyCode != 17) && (keyCode != 18)) {
+      textv += String.fromCharCode(key);
+    }
   } else {
-    textv = textv.substring(0, textv.length-2);
+    textv = textv.substring(0, textv.length-1);
   }
 };
 draw = function() {
   background(0);
   fill(255);
-  text(textv, 0, 0);
+  if ((frameCount % 60) < 30) {
+    text(textv + '|', 0, 0);
+  } else {
+    text(textv, 0, 0);
+  }
 };
 //end processingjs
   }
