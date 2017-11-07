ed227f389d33c4201b91fb2ec34f7521b8c215e4
coolguy284 <32657659+coolguy284@users.noreply.github.com>
2017-11-07 09:10:41 -0500

    Update countdowntime.js

--- countdowntime.js
+++ countdowntime.js
@@ -1,7 +1,7 @@
 var id, cd, dif, sd, nexteclipse;
 date = new Date();
-id = new Date(2017, 10, 10, 0, 0, 0);
+id = new Date(2017, 11, 7, 0, 0, 0);
 cd = new Date(date.getFullYear(), date.getMonth()+1, date.getDate(), 0, 0, 0);
 dif = Math.round(Math.abs((id.getTime() - cd.getTime())/86400000));
-sd = 51-dif*3;
+sd = 32-dif*3;
 nexteclipse = new Date('2024-04-08T17:59:17.000Z');
